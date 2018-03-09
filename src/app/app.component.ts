74d8a34c3b3274da165d8ef7349dbc92688eb812
Morgan Abel <morgan@mypalit.com>
2018-03-09 08:19:02 -0800

    breaks the build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as monaco from '@timkendrick/monaco-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+    let editor = monaco.editor.create(new HTMLElement, {
+      theme: "vs-dark",
+      language: "csharp"
+    });
+  }
 }
